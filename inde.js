723ef52c30b9b257cb94a2fb67f27afab7589e5f
Paulovit312 <142530761+Paulovit312@users.noreply.github.com>
2023-08-21 12:36:22 -0300

    Add files via upload

---
+++ inde.js
@@ -0,0 +1,8 @@
+console.log('Paulo "vitor" '); //string = texto
+console.log("Paulo vitor"); //number= numero
+console.log(`Paulo vitor`);
+
+console.log(123456, 15.85, 'paulo');
+
+
+console.log('Meu nome é "Paulo". Estou aprendendo JavaScript à', 10, 'da manhã.' );
