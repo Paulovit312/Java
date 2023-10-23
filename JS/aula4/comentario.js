260ee11050d266a0bcfe97ca181641afdad56068
Paulovit312 <142530761+Paulovit312@users.noreply.github.com>
2023-10-23 14:05:41 -0300

    f

---
+++ JS/aula4/comentario.js
@@ -0,0 +1,13 @@
+// esrever um comentario//
+console.log('Ola mundo');// Aqui tem outro comentario
+console.log('Linha1');
+
+/* 
+um bloco de notação dentro desses caracteris
+*/
+console.log('Linha1');
+console.log('Linha1');
+//console.log('Linha1');
+console.log('Linha1');
+console.log('Linha1');
+// vai ser ignorado
